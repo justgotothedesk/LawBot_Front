3f36046a34dbae0c0a0b918ab7fa103b7564fb84
Shin <justgotothedesk@naver.com>
2023-11-11 14:30:36 +0900

    ver. 5

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,13 +6,10 @@ const Header = () => {
     <div>
       <div className="header-container">
         <img src="logo.png" alt="logo" style={{ width: '100px', height: '100px' }} />
-        <h3 style={{ marginLeft: '10px' }}>ㅇㅇㅇ님</h3>
+        <h3 style={{ marginLeft: '10px' }}>환영합니다.</h3>
       </div>
       <div>
         <h2 style={{ fontSize: '1rem', position: 'sticky', top: '0', marginLeft : "150px"}}>죄잇나 ver. 1.0</h2>
-      </div>
-      <div className="history">
-        <h2 style={{fontSize: '1.2rem'}}>Chat History</h2>
       </div>
     </div>
   );
