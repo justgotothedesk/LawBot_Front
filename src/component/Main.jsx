a70553892289aad2f84444d3cb518d8f180862c2
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-11-12 14:36:21 +0900

    Update Main.jsx

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -26,7 +26,7 @@ const Main = () => {
   };
 
   const onSignUp = () => {
-    fetch('http://34.64.242.124:5000', {
+    fetch('http://34.64.242.154:5000', {
       method: 'POST',
       body: JSON.stringify({ question: input }),
       headers: {
